6cedd1dfa64bc07998cee1a691ddc56bf600f060
sdekrishan <107391017+sdekrishan@users.noreply.github.com>
2022-07-23 13:29:55 +0530

    complete all tasks

---
+++ pricing.js
@@ -0,0 +1,21 @@
+document.querySelector("#navbar>img").addEventListener("click",function (){
+    window.location.href="home.html"
+})
+
+
+document.querySelector("#chokor").addEventListener("click",function(){
+    window.location.href="https://railsware.com/"
+})
+
+
+document.querySelector("#login").addEventListener("click",function(){
+    window.location.href="loginpage.html"
+})
+
+
+// for signup button
+
+
+document.querySelector("#signup").addEventListener("click",function(){
+    window.location.href="signup.html"
+})
